bc4e16c2f7d5f4c3b4ca4f8e9f935e10a44c47f6
Arnett McMurray <arnettmcmurray@Arnetts-Mac-mini.local>
2025-07-08 23:01:23 -0500

    kata 2

--- lesson-6/codewars_solutions.py
+++ lesson-6/codewars_solutions.py
@@ -4,3 +4,17 @@
 def string_to_array(s):
     return s.split()
 
+#kata2 ----Given a list of numbers, return the sum of only the positive ones.
+#Ignore 0 and negatives.
+
+# ------https://www.codewars.com/kata/5715eaedb436cf5606000381
+#notes 
+# positive_sum([1, -4, 7, 12]) ➜ 20
+#positive_sum([-1, -2, -3, -4]) ➜ 0
+#positive_sum([]) ➜ 0
+def positive_sum(arr):
+    return sum(x for x in arr if x > 0)
+
+#Kata3
+
+
