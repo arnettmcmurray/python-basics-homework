d61cc72eb21f3c6db79f927ff3e8c546eab3fee6
Arnett McMurray <arnettmcmurray@Arnetts-Mac-mini.local>
2025-07-08 23:04:24 -0500

    kata 3

--- lesson-6/codewars_solutions.py
+++ lesson-6/codewars_solutions.py
@@ -1,4 +1,4 @@
-# 1. ------Convert a String to an Array------
+# Kata 1. ------Convert a String to an Array------
 # https://www.codewars.com/kata/57e76bc428d6fbc2d500036d
 
 def string_to_array(s):
@@ -7,7 +7,7 @@ def string_to_array(s):
 #kata2 ----Given a list of numbers, return the sum of only the positive ones.
 #Ignore 0 and negatives.
 
-# ------https://www.codewars.com/kata/5715eaedb436cf5606000381
+#-https://www.codewars.com/kata/5715eaedb436cf5606000381
 #notes 
 # positive_sum([1, -4, 7, 12]) ➜ 20
 #positive_sum([-1, -2, -3, -4]) ➜ 0
@@ -15,6 +15,15 @@ def string_to_array(s):
 def positive_sum(arr):
     return sum(x for x in arr if x > 0)
 
-#Kata3
+#Kata3 -----Given a list, return a new list with all duplicates removed.
+#Preserve the original order of first appearances.-------
+# https://www.codewars.com/kata/57a5b0dfcf1fa526bb000118
 
-
+def distinct(seq):
+    seen = set()
+    result = []
+    for item in seq:
+        if item not in seen:
+            seen.add(item)
+            result.append(item)
+    return result
